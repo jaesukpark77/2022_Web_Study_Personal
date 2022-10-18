3c1eac5285e1907d8dae422742a92b69401e0929
JAESEOK PARK <jaesukpark77@naver.com>
2022-10-18 22:02:35 +0900

    starbucks 프로젝트
    main.js - 공지사항 추가

--- K-Digital 기초역량훈련/실습/starbucks/js/main.js
+++ K-Digital 기초역량훈련/실습/starbucks/js/main.js
@@ -2,43 +2,51 @@ const searchEL = document.querySelector('.search');
 const searchInputEl = searchEL.querySelector('input');
 
 searchEL.addEventListener('click', function(){
-    searchInputEl.focus();
+  searchInputEl.focus();
 });
 
 searchInputEl.addEventListener('focus', function(){
-    searchEL.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
+  searchEL.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
 });
 
 searchInputEl.addEventListener('blur', function(){
-    searchEL.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
+  searchEL.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
 });
 
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function(){
-    console.log(window.scrollY);
-    if(window.scrollY>500){
-        // 배지 숨기기
-        // gsap.to(요소, 지속시간, 옵션);
-        gsap.to(badgeEl, .6, {
-            opacity: 0,
-            display: 'none'
-        })
-    } else{
-        // 배지 보이기
-        gsap.to(badgeEl, .6, {
-            opacity: 1,
-            display: 'block'
-        })
-    }
+  console.log(window.scrollY);
+  if(window.scrollY>500){
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  } else{
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
 }, 300));
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function(fadeEl, index){
-    gsap.to(fadeEl, 1, {
-        delay: (index + 1) * .7,
-        opacity: 1
-    });
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  autoplay:true,
+  loop:true
 });
